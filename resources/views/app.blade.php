dfd0ed7062687cec20c50ae63bc7d8778a936957
Logan <logannaranjo4@gmail.com>
2026-04-10 19:13:38 +0200

    MQR title modified

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'mqr') }}</title>
+        <title inertia>{{ strtoupper(config('app.name', 'MQR')) }}</title>
 
         <!-- Favicon -->
         <link rel="icon" type="image/svg+xml" href="/favicon.svg">
